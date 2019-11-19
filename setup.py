ebc05ee5b4300d68fd076ae772aabe781e195505
Saravanabalagi R <saravanabalagi@hotmail.com>
2019-11-19 12:16:05 +0000

    Bumping version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import pathlib
 from setuptools import setup
-from .imaugtools import __version__
+from imaugtools import __version__
 
 # The directory containing this file
 HERE = pathlib.Path(__file__).parent
@@ -12,7 +12,8 @@ README = (HERE / "README.md").read_text()
 setup(
     name="imaugtools",
     version=__version__,
-    description="imaugtools contains tools used for image augmentation: translate, rotate, crop. This library is for you if you do NOT want to stretch or skew, or pad pixels when doing any of these operations.",
+    description="imaugtools contains tools used for image augmentation: translate, rotate, crop. This library is for "
+                "you if you do NOT want to stretch or skew, or pad pixels when doing any of these operations.",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/saravanabalagi/imaugtools",
@@ -20,8 +21,8 @@ setup(
     author_email="saravanabalagi@hotmail.com",
     license="MIT",
     classifiers=[
-    	"Development Status :: 3 - Alpha",
-    	"Topic :: Software Development :: Libraries",
+        "Development Status :: 4 - Beta",
+        "Topic :: Software Development :: Libraries",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
